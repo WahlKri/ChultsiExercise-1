779d30b055ee422cc405fba1fdc71dc2f3686c84
jultsi <julia_80s@hotmail.com>
2014-06-26 17:31:08 +0300

    Jultsi did it :)

--- Starters.py
+++ Starters.py
@@ -6,40 +6,59 @@ __author__ = ''
 "-----------------------------------------------"
 
 "Print your name"
+print("Julia Poutanen")
 
 "Make a comment like this"
+# a comment like this
 
 "Use # to make a comment"
+"yyyyffi"
 
 "Print the sum of two numbers"
+print(4+6)
+
+print"Hens", 25 - 30 / 6
+
+print "Is it greater?", 5 > -2
+print "Is it less or equal?", 5 <= -2
+print 5 <= -2
+print "Roosters", 100 - 25 * 3 % 4
 
 "Modulo of 4 and 2? (Hint: 4 modulo 2 = 4-2-2 = 0)"
+print(55 % 10)
 
 "Is 5 greater than 2? Use '>' and print it"
+print("Is 5 greater than 2?", 5 > 2)
 
 "Is the sum of 5 and 5 equal to 11?"
+print(5+5 == 11)
 
 "5 is greater-or-equal than 5? (Hint: use >=)"
+print(5 >= 5)
 
 
 
-
-"Variables (when printing don't use any written numbers, use only variable values, so don't do like 
-"print("There are 100 people at the zoo"))"
+"Variables (when printing don't use any written numbers, use only variable values, so don't do like "
+"print("#There are 100 people at the zoo"))"
 
 "-----------------------------------------------"
 
 "Make variable 'people' and set its value to 100"
+people = 100
 
 "print the value so the outcome is 'There are 100 people at the zoo'"
+print "There are", people, "people at the zoo"
 
 "make a new value and name it 'got-eaten' and set its value to 25"
+got_eaten = 25
 
 "print 'But 25 of them got eaten by a monkey', but use only variables not '25' as written number"
+print "But", got_eaten, "of them got eaten by a monkey"
 
 "print 'Now there are 75 people left and that is 75% from the original'."
-
+print "Now there are", people - got_eaten, "people left and that is", (float)(people - got_eaten) / people * 100, "% from the original."
 
 "To be continued..."
 "-----------------------------------------------"
 
+print "Motmotmoot"
