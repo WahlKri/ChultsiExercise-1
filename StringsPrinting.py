e97ae6e06c5851ed46496c277e79b59707e05ad3
Kristian Wahlroos <kristian.wahlroos@gmail.com>
2014-06-26 21:35:50 +0300

    Update StringsPrinting.py

--- StringsPrinting.py
+++ StringsPrinting.py
@@ -1,2 +1,78 @@
 "Chapters 4-9"
 
+
+"Do the calculation of two different decimals. The output should be 0.425"
+
+
+"Copy next variables and fill them with your personal information. After that use %-operator to print strings" +
+"like so:  print("My name is -the operator- (your_variable)"). Hint: there are two different %-operators for strings"+
+"%s and %d figure out what they do and use the correct one."
+
+#my_name = ''
+#my_age = 
+#my_height = 
+#my_weight = 
+#my_eyes = ''
+#my_teeth = ''
+#my_hair = ''
+
+"What is %s used for?"
+
+"What is %d used for?"
+
+"Use % for every variable and print them out somehow (you can use any sentence you like)"
+
+"Calculate the sum of your weight, height and age using %-operator for strings. "+
+"For example 'My age is 10, my height is 160 and my weight is 50" 
+"Sum of those is 220"
+
+"Make a variable called 'truth' and set it to 42. Next make a variable called truth_sentence and use %-operator " +
+"to construct following sentence using the 'truth'-variable: 'the meaning of life is 42'" +
+"Next make a new variable called whole_sentence. The command print(whole_sentence) should produce " + 
+"I heard that the meaning of life is 42"
+
+#end1 = "C"
+#end2 = "h"
+#end3 = "e"
+#end4 = "e"
+#end5 = "s"
+#end6 = "e"
+#end7 = "B"
+#end8 = "u"
+#end9 = "r"
+#end10 = "g"
+#end11 = "e"
+#end12 = "r"
+
+"Copy those variables and construct a sentence using '+' and print(). Then print it 100 times " +
+"so that every word is seperated by a line (Hint: print("a\n"*10), prints 'a' ten times to different rows)" + 
+"\n means newline"
+
+"Make a variable 'formatter = %r %r %r %r' (%r means raw data and works as %-operator)." + 
+"Now use it to display numbers like this: '1 2 3 4'"
+
+"Make a variable that when called print(variable) prints out the next without '#':"
+
+#Feb
+#Mar
+#Apr
+#May
+#Jun
+#Jul
+#Aug"
+
+"Make a multiline comment using '"""', then use that to print: "
+
+#There's something going on here.
+#With the three double-quotes.
+#We'll be able to type as much as we like.
+#Even 4 lines if we want, or 5, or 6.
+
+
+
+
+
+
+
+
+
