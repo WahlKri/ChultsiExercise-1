25f41c2364d5091f3c2280ba48b51fc80b4bd33c
Kristian Wahlroos <kristian.wahlroos@gmail.com>
2014-06-27 13:52:41 +0300

    Update UserInput.py

--- UserInput.py
+++ UserInput.py
@@ -32,5 +32,53 @@ while True:
 
 "Next make a program that asks name, age and birthplace from the user. Save inputs to variables and print them out"
 
+"Make a calculator which asks for a two integers and plusses them together. For example "
+#Give number a: 
+#Give number b: 
+#The sum of a and b is result
+
+"Did you encounter any problems? Remember that input() produces raw data format/string. You must somehow turn that "
+"into integer. For example '(float) variable = 5', turns 5 into decimal 5.0. That is called casting"
+
+"What happens if you try input(5). Why this happens?"
+
+"Difference between %s and %r?"
+
+"What happens when you write input(), PyCharm gives more info about the function and it should say print(prompt),"
+"What do you think prompt means and how it could be used. Hint: input(\"Give a number\")"
+"Use the new way of writing a program that asks for a name, age and birthplace. It should also print the results out"
+
+"Read chapter 14 and answer to these questions."
+"What does import do? How about from?"
+"Try to import 'getsizeof' from sys and use it"
+
+"import argv from sys. And make a variable called 'path' and set its value to argv[0]. Then print the variable 'path'"
+"What happened?"
+
+
+"Make a program that works following way. You can use any structure you want, but the main idea is to ask "
+"couple of questions from the user and print them out"
+
+"""
+I'd like to ask you a few questions.
+What is your name?
+> Kalle (user input)
+Do you like me Kalle?
+>  Yes (user input)
+Where do you live Kalle?
+>  San Francisco (user input)
+What kind of computer do you have?
+>  Tandy 1000 (user input)
+
+Alright, so you said 'Yes' about liking me.
+You live in 'San Francisco'.  Not sure where that is.
+And you have a 'Tandy 1000' computer.  Nice.
+
+
+"""
+
+
+
+
 
 
